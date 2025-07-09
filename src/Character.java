315944053e596ca4a16a5857ab19acfbf8e55bfb
Yudai Mikami <241122@std.hi-joho.ac.jp>
2025-07-09 23:10:50 +0900

    作成中

---
+++ src/Character.java
@@ -0,0 +1,30 @@
+public abstract class Character implements Creature {
+    private String name;
+    private int hp;
+    //コンストラクタ
+    public Character(String name, int hp) {
+        this.name = name;
+        this.hp = Math.max(hp,0);
+    }
+    //生存確認
+    public final boolean isAlive() {
+        return getHp() > 0;
+    }
+    //HP,Name確認
+    public void showStatus() {
+        System.out.println(getName() + ": HP" + getHp());
+    }
+    //ゲッター＆セッター
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp() {
+        this.hp = Math.max(hp, 0);
+    }
+}
---
+++ src/Hero.java
@@ -0,0 +1,14 @@
+public class Hero extends Character {
+    private String weapon;
+
+    public Hero(String name, int hp, String weapon) {
+        super(name, hp);
+        this.weapon = weapon;
+    }
+    public void attack(Creature target){
+        System.out.println(getName() + "は" + weapon + "で攻撃！" + target.getName() + "に10のダメージを与えた！");
+        target.getHp() ;
+    }
+
+
+}
